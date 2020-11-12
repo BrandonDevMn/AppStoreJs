0e2575f28af21ef075019901d6cf0de2876059a5
BrandonDevMn <brandon.dev.mn@icloud.com>
2020-11-12 14:49:15 -0600

    settings up

--- js/view/index.js
+++ js/view/index.js
@@ -1,25 +1,30 @@
-var vue = null;
+// list of all meta.json files
 var urls = [
-	"https://brandondevmn.github.io/DiceJs/meta.json"
+	"https://brandondevmn.github.io/DiceJs/meta.json",
+	"https://brandondevmn.github.io/DiceJs2/meta.json"
 ];
 
+// called on load
 function onLoad() {
-	vue = new Vue({
-		el: '#app',
-		data: {
-			apps: []
-		}
-	});
-	
+	// create view
+
+	// loop over urls
 	for(var i = 0; i < urls.length; i++){
+		// for each url
 		var url = urls[i];
+
+		// pull data from urls
 		http("GET", url, function(response){
+			// parse the response
 			var json = JSON.parse(response);
-			vue.data.apps.push(json);
+
+			// add it to the view
+			// TODO
 		});
 	}
 }
 
+// simple http function
 function http(verb, url, onResponse){
 	var h = new XMLHttpRequest();
 	
